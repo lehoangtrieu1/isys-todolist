0c5714d7c20974c1a2a7ca5bf09695300e5ead0b
lehoangtrieu1 <127284462+lehoangtrieu1@users.noreply.github.com>
2024-04-15 09:30:04 +0800

    added formatted

--- js/script.js
+++ js/script.js
@@ -4,11 +4,22 @@ console.log("script Loaded");
 let addTaskButton = document.getElementById("add-button")
 let newTaskInput = document.getElementById("task-input")
 let todoContainer = document.getElementById("todo-list")
+let templateElement = document.getElementById("todo-item-template")
+let template = templateElement,innerHTML
 
 // 2. Write function to handle the events (read value -- clear the box-- read the value)
 function onAddTaskClicked(event) {
+    
+    //get the contents of the input box
     let taskName = newTaskInput.value;
+    
+    //clear the input box
     newTaskInput.value = ""
+
+    //search and replace to add task name (found above)
+    let todoHTML = template.replace(</!--TASK_NAME -->, taskName)
+
+    //we have the formatted HTML, lets insert it into the todo container
     todoContainer.insertAdjacentHTML('afterbegin', taskName)
 }
 
