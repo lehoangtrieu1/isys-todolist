9e2cdabda14a03d0a1b8008c24add51cc4c3f1fd
lehoangtrieu1 <127284462+lehoangtrieu1@users.noreply.github.com>
2024-04-15 08:56:50 +0800

    added text to DOm

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log("script Loaded");
+
+// 1. Assign variable to DOM elements
+let addTaskButton = document.getElementById("add-button");
+let newTaskInput = document.getElementById("task-input");
+let todoContainer = document.getElementById("todo-list")
+
+// 2. Write function to handle the events (read value -- clear the box-- read the value)
+function onAddTaskClicked(event) {
+    let taskName = newTaskInput.value;
+    newTaskInput.value = ""
+    todoContainer.insertAdjacentHTML('afterbegin', taskName)
+}
+
+// 3. link, element, event to the function
+addTaskButton.addEventListener('click',onAddTaskClicked)
